d957c5289dfea1082140499799dccd47ebfb1d55
liangsisheng <liangsish@gmail.com>
2022-10-31 01:40:18 -0400

    database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
-// use the express library
+
+//  use the express library
 const express = require('express');
 // create a new server application
 const app = express();
+//standard api for web requests
+const fetch = require('node-fetch');
 
+//
+//
 // cookie 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
@@ -35,7 +40,46 @@ app.get('/', (req, res) => {
   } /* params */);
 });
 
+app.get("/trivia", async (req, res) => {
+  // fetch the data
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
 
+  // fail if bad response
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+
+  // interpret the body as json
+  const data = await response.json();
+
+  // fail if db failed
+  if (data.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${data.response_code}`);
+    return;
+  }
+
+  // respond to the browser
+ // res.send(JSON.stringify(content, 2));
+  correctAnswer = data.results[0]['correct_answer'];
+  answers = data.results[0]['incorrect_answers'];
+  answers.push(correctAnswer);
+
+  const answerLinks = answers.map(answer => {
+  return `<a href="javascript:alert('${
+    answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+    }')">${answer}</a>
+  }`
+})
+
+  res.render('trivia', {question: data.results[0]['question'],
+    category: data.results[0]['category'],
+    difficulty: data.results[0]['difficulty'],
+    answers: answerLinks
+  })
+  });
 app.use(express.static('public'));
 // Start listening for network connections
 app.listen(port);
